a271f69202b749f2b10e99d251c778b1f5da99b5
AJTech1968 <40666123+AJTech1968@users.noreply.github.com>
2020-09-06 09:58:48 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'counter-app';
+  title = 'Counter App';
+
+  count = 0;
+
+  handleIncrease = () => {
+    if (this.count < 10) {
+      this.count = this.count + 1;
+    } else {
+      console.log('greter thsn 10 not allowed');
+    }
+
+  }
+
+  handleDecrease() {
+    if (this.count > 0) {
+      this.count = this.count - 1;
+    } else {
+      console.log('less thsn 0 not allowed');
+    }
+  }
+
+  handleReset() {
+    this.count = 0;
+  }
+
 }
